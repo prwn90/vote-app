cb60894d41e5e3da0ea007f7d0e10a3f10c2bf04
prwn90 <purwin.daniel@gmail.com>
2020-07-05 11:25:59 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+//Candidates 
+
+class App extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			candidates : [
+				{name: "Barack Obama", votes: 0},
+				{name: "Donald Trump", votes: 0},
+				{name: "George W. Bush", votes: 0},
+        {name: "Bill Clinton", votes: 0},
+        {name: "Joe Biden", votes: 0},
+        {name: "Hillary Clinton", votes: 0}
+			]
+		}
+	}
+
+
+  //Votes 
+  
+	vote (i) {
+		let newCandidates = [...this.state.candidates];
+		newCandidates[i].votes++;
+		function swap(array, i, j) {
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		this.setState({candidates: newCandidates});
+		
+	}
+
+	render(){
+		return(
+			<>
+				<h1>Choice Your President!</h1>
+				<div className="candidates">
+					{
+						this.state.candidates.map((candidate, i) => 
+							<div key={i} className="candidate">
+								<div className="voteCount">
+									{candidate.votes}
+								</div>
+								<div className="candidateName">
+									{candidate.name}
+								</div>
+								<button onClick={this.vote.bind(this, i)}>Vote!</button>
+							</div>
+						)
+					}
+				</div>
+			</>
+		);
+	}
+}
 export default App;
